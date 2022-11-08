f78f130a752d51ed7ba76e91b46fdce31a01836e
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-08 18:36:39 +0200

    Add Fragment example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
-    <Header />
+    <>
+      <p>123214</p>
+      <p>sdgfdgfgf</p>
+    </>
   );
 }
 
