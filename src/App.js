c60729b98e78998e702f82f530bd4cc1965d616d
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-08 19:23:46 +0200

    Add elements and props conditional render example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import './App.css';
 import Greeting from './components/Greeting';
 import Header from './components/Header';
 
+const user = {
+  id: 7452552,
+  name: 'User',
+  role: 'user',
+};
+
+const admin = {
+  id: 123,
+  role: 'admin',
+};
+
 function App() {
+  // v1
+  // if (user.role === 'admin') {
+  //   return (
+  //     <>
+  //       <Greeting name="Anton" />
+  //       <Greeting />
+  //     </>
+  //   );
+  // }
+
+  // v2
+  // return admin.role === 'admin' && <Greeting />;
+
   return (
     <>
-      <Greeting name="Anton" />
-      <Greeting />
+      {user.role === 'admin' && <Greeting name={user.name} />}
+      {admin.role === 'admin' && <Greeting name={admin.name || "Test" } />}
+      <input disabled={user.role === 'admin'} />
     </>
   );
 }
