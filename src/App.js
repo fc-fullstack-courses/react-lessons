8ba661f51a9229faf06b9c74481a70e734496a4c
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-10 19:50:34 +0200

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import './App.css';
-import Aloha from './components/Aloha';
-import AlohaList from './components/AlohaList';
-import Greeting from './components/Greeting';
-import Header from './components/Header';
-
-const user = {
-  id: 7452552,
-  name: 'User',
-  role: 'user',
-};
-
-const admin = {
-  id: 123,
-  name: 'Admin',
-  role: 'admin',
-};
+import Clicker from './components/Clicker';
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <AlohaList />
-      </>
+      <main>
+        <Clicker />
+      </main>
     );
   }
 }
--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 /*
   Сделать компонент кликера
   он должен отображать счетчик лкиокв
@@ -8,5 +9,46 @@
   Создайте компонент КонтроллерКликера - туда должна поехать кнопка
 
   Кликер должен работать как обычно
-
 */
+
+class Clicker extends Component {
+  state = {
+    counter: 0,
+  };
+
+  handleClick = () => {
+    const { counter } = this.state;
+
+    const newState = {
+      counter: counter + 1,
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <ClickerDisplay clicks={counter} />
+        <ClickerController onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+function ClickerDisplay(props) {
+  const { clicks } = props;
+  return (
+    <div>
+      <p>You have made {clicks} clicks</p>
+    </div>
+  );
+}
+
+function ClickerController(props) {
+  const { onClick } = props;
+  return <button onClick={onClick}>Click me</button>;
+}
+
+export default Clicker;
