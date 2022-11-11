0a7d83575060d1426c5a01dc8a988b35da5ed85b
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-11 18:43:14 +0200

    Add setState callback

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Timer extends Component {
   state = {
     number: 10,
+    isDoubled: true,
   };
 
   constructor(props) {
@@ -10,14 +11,24 @@ class Timer extends Component {
   }
 
   decrementNumber = () => {
-    const { number } = this.state;
-    if (number === 0) {
-      clearInterval(this.timerId);
-    } else {
-      this.setState({
-        number: number - 1,
+    const { number, isDoubled } = this.state;
+    // if (number === 0) {
+    //   clearInterval(this.timerId);
+    // } else {
+    this.setState((state) => ({
+      number: state.number - 1,
+    }));
+    if (isDoubled) {
+      console.log('Doubled');
+      // когда надо самому свежему состоянию
+      this.setState((updatedState) => {
+        const newState = {
+          number: updatedState.number - 1,
+        };
+        return newState;
       });
     }
+    // }
   };
 
   componentDidMount() {
@@ -43,6 +54,7 @@ class Timer extends Component {
     return (
       <div>
         <p>{number}</p>
+        <button onClick={this.decrementNumber}>Decrement</button>
       </div>
     );
   }
