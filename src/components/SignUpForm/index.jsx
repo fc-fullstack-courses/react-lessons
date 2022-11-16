d70160075136c12982bea978aa6f95c7a6bc29a6
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-16 19:48:00 +0200

    Add controlled input

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import styles from './SignUpForm.module.scss';
 
 class SignUpForm extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   submitHandler = (e) => {
     e.preventDefault();
 
-    console.log(e.target.elements.email.value); // email
-    console.log(e.target.password.value); // password
+    // console.log(e.target.elements.email.value); // email
+    // console.log(e.target.password.value); // password
+  };
+
+  handleEmailChange = (e) => {
+    // console.log(e.target.value);
+    this.setState({
+      email: e.target.value
+    })
   };
 
   render() {
+    const { email, password } = this.state;
     return (
       <form className={styles.form} onSubmit={this.submitHandler}>
-        <input className={styles.input} type="email" name="email" />
+        <input
+          className={styles.input}
+          type="email"
+          name="email"
+          value={email}
+          onChange={this.handleEmailChange}
+        />
         <input className={styles.input} type="password" name="password" />
         <button className={styles.btn} type="submit">
           Sign Up
