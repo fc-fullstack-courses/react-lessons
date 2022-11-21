049f460e82328d5b243d287babe5314703d85be9
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-21 19:33:32 +0200

    refactor getUsers

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,22 @@
-export function getUsers(currentPage) {
-  const promise = fetch(
-    `https://randomuser.me/api/?page=${currentPage}&results=10&seed=foobarbaz&nat=ua&inc=gender,name,location,email,login`
-  )
+import queryString from 'query-string';
+
+export function getUsers(options) {
+  const defaultOptions = {
+    page: 1,
+    results: 10,
+    seed: 'asdsafdsfdfds',
+    nat: 'ua',
+    inc: ['gender', 'name', 'location', 'email', 'login'],
+  };
+
+  const finalOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  const query = queryString.stringify(finalOptions, {arrayFormat: 'comma'});
+
+  const promise = fetch(`https://randomuser.me/api/?${query}`)
     .then((res) => res.json())
     .then((data) => data.results);
 
