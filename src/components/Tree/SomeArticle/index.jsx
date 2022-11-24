ca16952d27454be5010015f4d5c317b593138547
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-24 19:22:26 +0200

    Add basic HOC

--- src/components/Tree/SomeArticle/index.jsx
+++ src/components/Tree/SomeArticle/index.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
 import { ProductContext, ThemeContext } from '../../../contexts';
-import CONSTANTS from '../../../constants'; 
+import CONSTANTS from '../../../constants';
 const { THEMES } = CONSTANTS;
 
 const SomeArticle = (props) => {
-  // const styles = {
-  //   background: theme ? 'red': 'black'
-  // }
-
+  const { product } = props;
   return (
-    <ThemeContext.Consumer>
-      {([theme, onClick]) => {
-        const styles = {
-          backgroundColor: theme === THEMES.LIGHT ? 'lightgray' : '#444444',
-          color: theme === THEMES.LIGHT ? 'black' : 'white',
-        };
-
-        return (
-          <ProductContext.Consumer>
-            {(product) => (
-              <article style={styles}>
-                <h5>Article</h5>
-                <button onClick={onClick}>Change Theme</button>
-                <div>{JSON.stringify(product)}</div>
-              </article>
-            )}
-          </ProductContext.Consumer>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <article>
+      <h5>Article</h5>
+      <button onClick={() => {}}>Change Theme</button>
+      <div>{JSON.stringify(product)}</div>
+    </article>
   );
 };
 
-export default SomeArticle;
+// function SomeArticleWithProduct({ theme, onClick }) {
+//   return (
+//     <ProductContext.Consumer>
+//       {(product) => {
+//         const styles = {
+//           backgroundColor: theme === THEMES.LIGHT ? 'lightgray' : '#444444',
+//           color: theme === THEMES.LIGHT ? 'black' : 'white',
+//         };
+
+//         return (
+//           <article style={styles}>
+//             <h5>Article</h5>
+//             <button onClick={onClick}>Change Theme</button>
+//             <div>{JSON.stringify(product)}</div>
+//           </article>
+//         );
+//       }}
+//     </ProductContext.Consumer>
+//   );
+// }
+
+function withProduct(Component) {
+  function NewComponent(props) {
+    return (
+      <ProductContext.Consumer>
+        {(product) => <Component product={product} />}
+      </ProductContext.Consumer>
+    );
+  }
+
+  return NewComponent;
+}
+
+const SomeArticleWithProduct = withProduct(SomeArticle);
+
+export default SomeArticleWithProduct;
