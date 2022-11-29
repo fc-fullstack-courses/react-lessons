2b364232993478bbf7848bb01b50ce0763c847c9
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-29 20:20:46 +0200

    Add some exotic refs usage examples

--- src/components/RefClicker/index.jsx
+++ src/components/RefClicker/index.jsx
@@ -1,13 +1,33 @@
 import { useClicker } from 'hooks';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const RefClicker = (props) => {
   const elemRef = useRef();
   const clicks = useClicker(elemRef);
+  const inputRef = useRef();
+  const prevClicks = useRef(clicks);
+  const renders = useRef(1);
+
+  useEffect(()=> {
+    renders.current = renders.current + 1
+  })
+
+  useEffect(() => {
+    prevClicks.current = clicks;
+  }, [clicks]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
 
   return (
     <div id="test">
       <h1>Clicks: {clicks}</h1>
+      <p>Prev clicks {prevClicks.current}</p>
+      <p>Renders {renders.current}</p>
+      <input ref={inputRef} type="text" />
       <div
         ref={elemRef}
         style={{
