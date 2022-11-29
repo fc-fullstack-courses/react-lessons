54c6b211344b71b1456ee66835c55f753558fa62
Kyrylo <fresh.js.ftd.@gmail.com>
2022-11-29 20:39:31 +0200

    Add useCallback

--- src/components/RefClicker/index.jsx
+++ src/components/RefClicker/index.jsx
@@ -1,16 +1,18 @@
 import { useClicker } from 'hooks';
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 const RefClicker = (props) => {
+  const [value, setValue] = useState('');
+
   const elemRef = useRef();
-  const clicks = useClicker(elemRef);
   const inputRef = useRef();
+  const clicks = useClicker(elemRef);
   const prevClicks = useRef(clicks);
   const renders = useRef(1);
 
-  useEffect(()=> {
-    renders.current = renders.current + 1
-  })
+  useEffect(() => {
+    renders.current = renders.current + 1;
+  });
 
   useEffect(() => {
     prevClicks.current = clicks;
@@ -22,12 +24,23 @@ const RefClicker = (props) => {
     }
   }, [inputRef]);
 
+  // const handleChange = (e) => setValue(e.target.value);
+
+  const handleChange = useCallback((e) => setValue(e.target.value), []);
+
+  useEffect(() => {
+    console.log('handleChange created');
+  }, [handleChange]);
+
+  const logValue = () => console.log(`value is ${value}`);
+
   return (
     <div id="test">
       <h1>Clicks: {clicks}</h1>
       <p>Prev clicks {prevClicks.current}</p>
       <p>Renders {renders.current}</p>
-      <input ref={inputRef} type="text" />
+      <input ref={inputRef} type="text" value={value} onChange={handleChange} />
+      <button onClick={logValue}>Log value</button>
       <div
         ref={elemRef}
         style={{
