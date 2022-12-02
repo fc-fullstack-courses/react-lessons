d6e492c6774ec15e2442f8acaf82c1f911a72682
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-02 18:52:28 +0200

    Add validation examples

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -9,13 +9,53 @@ const SIGN_UP_SCHEMA = yup.object({
   password: yup.string().min(8).required(),
 });
 
+const user1 = {
+  email: 12345,
+  password: 'truepass',
+  lastName: 'Test',
+};
+
+const user2 = {
+  email: 'test@test.com',
+  password: '1234Test',
+  firstName: 'Test',
+  lastName: 'Null',
+};
+
+const isValid1 = SIGN_UP_SCHEMA.isValidSync(user1);
+const isValid2 = SIGN_UP_SCHEMA.isValidSync(user2);
+
+console.log(isValid1);
+console.log(isValid2);
+
+try {
+  console.log(user1);
+  const validate1 = SIGN_UP_SCHEMA.validateSync(user1, {
+    strict: true,
+    abortEarly: false,
+  });
+  console.log(validate1);
+} catch (error) {
+  console.dir(error);
+}
+
+try {
+  const validate2 = SIGN_UP_SCHEMA.validateSync(user2, {
+    strict: true,
+    abortEarly: false,
+  });
+  console.log(validate2);
+} catch (error) {
+  console.dir(error);
+}
+
 function SignUpForm(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const submitHandler = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <form className={styles.form} onSubmit={submitHandler}>
