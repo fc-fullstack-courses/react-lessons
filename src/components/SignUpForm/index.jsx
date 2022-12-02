7d1cf8268f28e0707b4ebc044c202b94ffe1c108
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-02 20:33:12 +0200

    Add Form component

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Field } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import { SIGN_UP_SCHEMA } from 'utils/validators/validationShemas';
 import styles from './SignUpForm.module.scss';
 
@@ -22,34 +22,30 @@ function SignUpForm(props) {
 
   return (
     <Formik initialValues={initialState} onSubmit={submitHandler}>
-      {(formikProps) => {
-        return (
-          <form className={styles.form} onSubmit={formikProps.handleSubmit}>
-            <Field className={styles.input} type="text" name="name" />
-            <Field className={styles.input} type="email" name="email" />
-            <Field className={styles.input} type="password" name="password" />
-            <Field as="select" name="accountLevel">
-              <option value="basic">Basic</option>
-              <option value="pro">Professional</option>
-              <option value="max">Maximal</option>
-            </Field>
-            <label>
-              <Field type="radio" name="gender" value="male" /> Male
-            </label>
-            <label>
-              <Field type="radio" name="gender" value="female" />
-              Female
-            </label>
-            <label>
-              <Field type="checkbox" name="isSubscribed" />
-              Subscribe to news
-            </label>
-            <button className={styles.btn} type="submit">
-              Sign Up
-            </button>
-          </form>
-        );
-      }}
+      <Form className={styles.form}>
+        <Field className={styles.input} type="text" name="name" />
+        <Field className={styles.input} type="email" name="email" />
+        <Field className={styles.input} type="password" name="password" />
+        <Field as="select" name="accountLevel">
+          <option value="basic">Basic</option>
+          <option value="pro">Professional</option>
+          <option value="max">Maximal</option>
+        </Field>
+        <label>
+          <Field type="radio" name="gender" value="male" /> Male
+        </label>
+        <label>
+          <Field type="radio" name="gender" value="female" />
+          Female
+        </label>
+        <label>
+          <Field type="checkbox" name="isSubscribed" />
+          Subscribe to news
+        </label>
+        <button className={styles.btn} type="submit">
+          Sign Up
+        </button>
+      </Form>
     </Formik>
   );
 }
