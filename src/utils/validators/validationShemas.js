eb865d17219b40122f9696094cedbe85ef587e5e
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-02 19:04:17 +0200

    Add email schema

--- src/utils/validators/validationShemas.js
+++ src/utils/validators/validationShemas.js
@@ -1,8 +1,15 @@
 import * as yup from 'yup';
 
+const EMAIL_SCHEMA = yup.string().email().required();
+
 export const SIGN_UP_SCHEMA = yup.object({
   firstName: yup.string().min(1).required(),
   lastName: yup.string().min(1).required(),
-  email: yup.string().email().required(),
+  email: EMAIL_SCHEMA,
+  password: yup.string().min(8).required(),
+});
+
+export const LOGIN_SCHEMA = yup.object({
+  email: EMAIL_SCHEMA,
   password: yup.string().min(8).required(),
 });
